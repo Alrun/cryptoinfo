e71545cbd0d003237efeef382622326d8b13335c
Alrun-PC <fkhey2016@yandex.ru>
2019-08-18 14:20:49 +0300

    Updates

---
+++ precache-manifest.93cf080599895cfe3b3603dfc8fb163c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481b32c901e505861373847e64f44c3e",
+    "url": "/cryptoinfo/build/index.html"
+  },
+  {
+    "revision": "d6d1235111e5a611f8d4",
+    "url": "/cryptoinfo/build/static/css/main.9f2dc881.chunk.css"
+  },
+  {
+    "revision": "7ceaa921528586fecdf0",
+    "url": "/cryptoinfo/build/static/js/2.97fb7bfd.chunk.js"
+  },
+  {
+    "revision": "d6d1235111e5a611f8d4",
+    "url": "/cryptoinfo/build/static/js/main.f2551d3d.chunk.js"
+  },
+  {
+    "revision": "dfe6d54df0a0cb4a0fa1",
+    "url": "/cryptoinfo/build/static/js/runtime~main.3b77393e.js"
+  }
+]);
