0e8043a9bd2335a8cd16d6de040d01d8b270ab00
Alrun-PC <fkhey2016@yandex.ru>
2019-08-18 13:16:10 +0300

    add build

---
+++ build/precache-manifest.e0504638ac42defaa67db692931d8041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898b7fb1f87700f541e6ef8b43ee777e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "767293d4370f6658d7a1",
+    "url": "/static/css/main.9f2dc881.chunk.css"
+  },
+  {
+    "revision": "7ceaa921528586fecdf0",
+    "url": "/static/js/2.97fb7bfd.chunk.js"
+  },
+  {
+    "revision": "767293d4370f6658d7a1",
+    "url": "/static/js/main.6c333378.chunk.js"
+  },
+  {
+    "revision": "9000508ed8e90ab5ceee",
+    "url": "/static/js/runtime~main.7bd5caa6.js"
+  }
+]);
