200a15c0db4c0daf6cef8140478957e7a36a1320
Alrun-PC <fkhey2016@yandex.ru>
2020-10-05 20:54:29 +0300

    Add build

---
+++ build/precache-manifest.600422fad81b58d32937938549f01ab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52959fa92dd6862e4b70c080838ab6af",
+    "url": "/cryptoinfo/index.html"
+  },
+  {
+    "revision": "cf731aee4e0f8142498f",
+    "url": "/cryptoinfo/static/js/2.35f3a81c.chunk.js"
+  },
+  {
+    "revision": "763b651d4309e9de60cb0cb63e631cbb",
+    "url": "/cryptoinfo/static/js/2.35f3a81c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5706de7e944eb87ed97",
+    "url": "/cryptoinfo/static/js/main.c6147da3.chunk.js"
+  },
+  {
+    "revision": "334d0365d76fdd71affc",
+    "url": "/cryptoinfo/static/js/runtime-main.c3363f89.js"
+  }
+]);
